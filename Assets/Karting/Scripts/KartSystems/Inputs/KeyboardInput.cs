a6f961973f73cbac443724aec6c90ecec931d1da
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-04-26 17:48:49 +0100

    Add random direction change

--- Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -7,6 +7,13 @@ namespace KartGame.KartSystems
         [Header("Turn Input")]
         public string TurnInputName = "Horizontal";
         public bool Reversed;
+        public bool RandomDirectionChange;
+
+        [Range(-1, 1)]
+        public float DirectionChangeProbability;
+        [Tooltip("The amount of time between between direction change reevaluation")]
+        public float TimeRemaining = 10.0f;
+        private float _currentTime;
 
         [Space]
 
@@ -23,6 +30,24 @@ namespace KartGame.KartSystems
                 Brake = Input.GetButton(BrakeButtonName),
                 TurnInput = Reversed ? turnInput * -1 : turnInput
             };
+        }
+
+        private void Update()
+        {
+            _currentTime += Time.deltaTime;
+
+            if (!(_currentTime >= TimeRemaining))
+            {
+                return;
+            }
+
+            if (!RandomDirectionChange || Random.value < DirectionChangeProbability)
+            {
+                return;
+            }
+
+            Reversed = !Reversed;
+            _currentTime = 0;
         }
     }
 }
